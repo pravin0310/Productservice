3c242dd3e38320e791883ead0f0aedd7f44c1ca3
pravinraj <pravinraj0101@gmail.com>
2025-05-14 01:24:19 +0530

    Initial Commit & Controller & Model created

---
+++ src/main/java/org/com/productservice/models/BaseModel.java
@@ -0,0 +1,10 @@
+package org.com.productservice.models;
+
+import java.util.Date;
+
+public class BaseModel {
+    private Long id;
+    private Date createAt;
+    private Date lastModifiedAt;
+
+}
---
+++ src/main/java/org/com/productservice/models/Category.java
@@ -0,0 +1,5 @@
+package org.com.productservice.models;
+
+public class Category extends BaseModel{
+    private String title;
+}
