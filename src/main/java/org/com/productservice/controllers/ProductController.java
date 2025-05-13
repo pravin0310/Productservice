a6e1fd4114b56f5cce8cb5dcc8d5d6ed096e0114
pravinraj <pravinraj0101@gmail.com>
2025-05-14 01:50:27 +0530

    Integrating Productcontroller 3rd party Apis

--- src/main/java/org/com/productservice/controllers/ProductController.java
+++ src/main/java/org/com/productservice/controllers/ProductController.java
@@ -1,9 +1,32 @@
 package org.com.productservice.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.com.productservice.models.Product;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable("id") Long id){
+        return new Product();
+    }
+
+    @GetMapping("/")
+    public List<Product> getAllProducts(){
+        return new ArrayList<>();
+    }
+
+    @PostMapping("/")
+    public Product createProduct(@RequestBody Product product){
+        return product;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable("id") Long id){
+        System.out.println("Deleteed product with id:" + id);
+    }
 }
